03067fd90d5ddb547e209b5a743f178f8ad52420
Pedro Augusto <pedroagustopdi@gmail.com>
2026-08-31 21:40:02 -0300

    feat: cria model de profissional

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profissional(User):
+    telefone = models.CharField(max_length=20)
+    cidade = models.CharField(max_length=100)
+    descricao = models.TextField()
+
+    foto = models.ImageField(
+        upload_to='profissionais/',
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.get_full_name() or self.username
